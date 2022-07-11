6b6613c88aa9b34bd95e0c2950d9f487a0d03671
Lluis Mnd <lluismf88@gmail.com>
2022-07-11 16:16:50 +0200

    Create Header.vue

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="header">
+        <div>DOT WATER</div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#header {
+    background-color: $main;
+    position: fixed;
+    width: 100%;
+    height: $menu-top-height;
+    top: 0;
+}
+</style>
