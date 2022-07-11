44d0ce4e1937cb7a78c5f659a98a2985768ffcd4
Lluis Mnd <lluismf88@gmail.com>
2022-07-11 18:32:04 +0200

    Added Maps JS Library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import * as VueGoogleMaps from "vue2-google-maps";
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: process.env.VUE_APP_API_MAP,
+        libraries: "places,visualization", // This is required if you use the Autocomplete plugin
+        // OR: libraries: 'places,drawing'
+        // OR: libraries: 'places,drawing,visualization'
+        // (as you require)
+
+        //// If you want to set the version, you can do so:
+        // v: '3.26',
+    },
+});
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,11 +1,54 @@
 <template>
-    <div id="map"></div>
+    <div id="map">
+        <GmapMap
+            :center="center"
+            :zoom="7"
+            map-type-id="terrain"
+            style="width: 100%; height: 100%"
+            :options="mapOptions"
+        >
+            <GmapMarker
+                :key="index"
+                v-for="(m, index) in markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="false"
+                @click="center = m.position"
+            />
+        </GmapMap>
+    </div>
 </template>
 
 <script>
 export default {
     name: "HomeView",
     components: {},
+    data() {
+        return {
+            mapOptions: {
+                zoomControl: true,
+                mapTypeControl: false,
+                scaleControl: false,
+                streetViewControl: false,
+                rotateControl: false,
+                fullscreenControl: true,
+                disableDefaultUI: false,
+            },
+            center: { lat: 10, lng: 10 },
+        };
+    },
+    computed: {
+        markers() {
+            return [
+                {
+                    position: { lat: 30, lng: 30 },
+                },
+                {
+                    position: { lat: 10, lng: 10 },
+                },
+            ];
+        },
+    },
 };
 </script>
 <style lang="scss" scoped>
